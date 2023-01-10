b83cab149e107c4f83533fabb23b5f31b3f56605
odek86 <116412189+odek86@users.noreply.github.com>
2023-01-10 20:53:10 +0700

    Add files via upload

--- dialogs/adddialog.cpp
+++ dialogs/adddialog.cpp
@@ -4,32 +4,26 @@ AddDialog::AddDialog() : QDialog(), ui(new Ui::AddDialog) {
     ui->setupUi(this);
     this->setFixedSize(width(), height());
     QClipboard *k = QApplication::clipboard();
-    tempUrl = k->text();
+    QUrl tempUrl = k->text();
 
-    QNetworkAccessManager* nam = new QNetworkAccessManager(this);
-    QNetworkReply *reply = nam->head(QNetworkRequest(tempUrl));
-    connect(reply, SIGNAL(error(QNetworkReply::NetworkError)),
-            this, SLOT(replyFinished(QNetworkReply::NetworkError)));
+    if(tempUrl.isValid()) {
+        qDebug("URL: %s", qUtf8Printable(tempUrl.toString()));
 
-    ui->lineEdit->setText(tempUrl);
-    ui->btnOK->setEnabled(true);
+        ui->txtName->setText(tempUrl.fileName());
+        ui->lineEdit->setText(tempUrl.toString());
+        ui->btnOK->setEnabled(true);
 
-    ui->txtSave->setText(getOutDirectory());
-}
-
-void AddDialog::replyFinished(QNetworkReply::NetworkError network) {
-    if (network != 0)     {
-        ui->lineEdit->setText("");
-        ui->btnOK->setEnabled(false);
+        ui->txtSave->setText(getOutDirectory());
     }
 }
+
+
 
 AddDialog::~AddDialog() {
     delete ui;
 }
 
 void AddDialog::on_btnOK_clicked() {
-    url = ui->lineEdit->text();
     fileGlobal = new DownloadFile(url);
     fileGlobal->resumable = ui->checkBox->isChecked();
     fileGlobal->miscArgs = ui->lineEdit_2->text();
@@ -43,6 +37,12 @@ void AddDialog::on_btnOK_clicked() {
 }
 
 void AddDialog::on_lineEdit_textChanged(const QString &arg1) {
+    url = ui->lineEdit->text();
+    if (QUrl(url).isValid()){
+        ui->txtName->clear();
+        QString fileName(QUrl(url).fileName());
+        ui->txtName->setText(fileName);
+    }
     ui->btnOK->setEnabled(QUrl(arg1).isValid());
 }
 
@@ -77,3 +77,9 @@ void AddDialog::on_chkName_clicked() {
 void AddDialog::on_txtName_textChanged(const QString &arg1) {
     ui->btnOK->setEnabled(!arg1.isEmpty());
 }
+
+void AddDialog::on_lineEdit_editingFinished()
+{
+
+}
+
